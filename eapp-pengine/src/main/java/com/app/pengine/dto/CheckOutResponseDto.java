a4dc537dc905435384db4d831bf570239c3618b7
manish9752 <manish9752@gmail.com>
2022-03-21 17:21:11 +0100

    Update CheckOutResponseDto.java

--- eapp-pengine/src/main/java/com/app/pengine/dto/CheckOutResponseDto.java
+++ eapp-pengine/src/main/java/com/app/pengine/dto/CheckOutResponseDto.java
@@ -1,36 +1,18 @@
 package com.app.pengine.dto;
-
 import java.util.List;
-
 import com.app.pengine.model.CheckOut;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
 public class CheckOutResponseDto   {
 	
 	private List<CheckOut> checkOut;
 	private int FinalPrice;
-	 
-	 
-	public List<CheckOut> getCheckOut() {
-		return checkOut;
-	}
-	public void setCheckOut(List<CheckOut> checkOut) {
-		this.checkOut = checkOut;
-	} 
-	public int getFinalPrice() {
-		return FinalPrice;
-	} 
-	public void setFinalPrice(int finalPrice) {
-		FinalPrice = finalPrice;
-	}
-	public CheckOutResponseDto(List<CheckOut> checkOut, int finalPrice) {
-		this.checkOut = checkOut;
-		FinalPrice = finalPrice;
-	}
-	public CheckOutResponseDto() {
-		 
-	}
-	
-	
-	
 
 }
